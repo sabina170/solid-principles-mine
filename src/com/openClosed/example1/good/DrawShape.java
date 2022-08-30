7f620de3570d2d9e8d33c22cc43e981294fa4ec8
Sabina Omarova <sabomar170@gmail.com>
2022-08-30 13:03:26 -0400

    2. openClosed - good code - example1 is created

---
+++ src/com/openClosed/example1/good/DrawShape.java
@@ -0,0 +1,15 @@
+package com.openClosed.example1.good;
+
+import java.util.List;
+
+public class DrawShape {
+
+    public void startDrawing(List<Shape> shapes) {
+
+        for (Shape shape: shapes) {
+            shape.draw();
+        }
+
+    }
+
+}
---
+++ src/com/openClosed/example1/good/Shape.java
@@ -0,0 +1,7 @@
+package com.openClosed.example1.good;
+
+public interface Shape {
+
+    void draw();
+
+}
